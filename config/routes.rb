6725c03b7d00aa75b3fe8555a48503e346b2f2b4
Steve Buehler <steve.buehler@gmail.com>
2018-01-21 22:07:09 +0000

    Disabled new user creation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions
-  resources :users
+  # comment out once users have been created
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
